ea3051f90ed5caa70de2c37b2d7aaebf223282e3
Swarup-Moulik <swarupmoulik1@gmail.com>
2025-03-20 21:03:28 +0530

    Update telephone_bill.py

--- telephone_bill.py
+++ telephone_bill.py
@@ -3,7 +3,7 @@ if calls <= 100:
     bill = 100
     print("Bill :- ", bill)
 elif 100 < calls <= 500:
-    bill = 100 + (calls/50) * 5
+    bill = 100 + (calls-100/50) * 5
     print("Bill :- ", bill)
 else:
     bill = 100 + (400/50) * 5 + (calls-500)*1.25
